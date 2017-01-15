0738b5035b93aed7735632636b8ae2cf174ceffc
Kjell Breiland <kjell@Kjells-MBP.norgenet.net>
2017-01-15 09:55:18 -0800

    links

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/kjbreil/pubinit/master/init.sh)"
+
 rm -fR ~/.norge
 git clone https://github.com/kjbreil/init.git ~/.norge
 cd ~/.norge
 
 ./ssh.sh
 ./zsh.sh
+./links.sh
