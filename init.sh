2374d84f211d9905ed8626a70aff7c6c1c87d7cc
Kjell Breiland <kjell.breiland@gmail.com>
2017-01-15 14:25:43 -0800

    home

--- init.sh
+++ init.sh
@@ -2,11 +2,12 @@
 
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/kjbreil/pubinit/master/init.sh)"
 
-rm -fR ~/.norge
-git clone https://github.com/kjbreil/init.git ~/.norge
+rm -fR $HOME/.norge
+git clone https://github.com/kjbreil/init.git $HOME/.norge
 
 
 $HOME/.norge/ssh.sh
 $HOME/.norge/zsh.sh
 $HOME/.norge/links.sh
 
+# git remote set-url origin git@github.com:kjbreil/init.git
