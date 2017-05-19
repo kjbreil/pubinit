3a8dda5a919c8b8db697d7bbe816c9aaaf43ffca
Kjell <kjell.breiland@gmail.com>
2017-05-18 20:45:02 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@
 rm -fR $HOME/.norge
 git clone https://github.com/kjbreil/init.git $HOME/.norge
 
-$HOME/.norge/pre-zsh.sh
+$HOME/.norge/bin/pre-zsh.sh
 
 # git remote set-url origin git@github.com:kjbreil/init.git
