5884baf4412d1b7e22b42d3706ee7e151131654b
Kjell <kjell.breiland@gmail.com>
2019-01-26 13:15:52 -0800

    Update unix.sh

--- unix.sh
+++ unix.sh
@@ -3,7 +3,7 @@
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/kjbreil/pubinit/master/unix.sh)"
 
 rm -fR $HOME/.norge
-git clone https://github.com/kjbreil/init.git $HOME/.norge
+git clone --recurse-submodules -j8 https://github.com/kjbreil/init.git $HOME/.norge
 
 $HOME/.norge/bin/pre-zsh.sh
 
